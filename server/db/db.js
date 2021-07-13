7316ce3b35da7db3a46d57da0d1aa8d94547b3cb
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2021-07-13 12:11:34 -0700

    added some config to Sequelize in db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+const pass = process.env.SESSION_SECRET;
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://postgres:${pass}@localhost:5432/messenger`, {
   logging: false
 });
 
